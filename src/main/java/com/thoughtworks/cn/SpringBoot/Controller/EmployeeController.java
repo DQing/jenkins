780dc4a184f192a9df3e1a9e784176e30f231d38
DQing <15829258737@163.com>
2018-05-27 12:20:54 +0800

    modify
    Signed-off-by: DQing <15829258737@163.com>

--- src/main/java/com/thoughtworks/cn/SpringBoot/Controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/cn/SpringBoot/Controller/EmployeeController.java
@@ -1,12 +1,7 @@
 package com.thoughtworks.cn.SpringBoot.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api")
