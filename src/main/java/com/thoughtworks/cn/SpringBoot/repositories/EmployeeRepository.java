0da56d494f9ff89ae22591a978674642f720ada5
DQing <15829258737@163.com>
2018-05-27 11:59:19 +0800

    init success
    Signed-off-by: DQing <15829258737@163.com>

--- src/main/java/com/thoughtworks/cn/SpringBoot/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks.cn.SpringBoot.repositories;
-
-import com.thoughtworks.cn.SpringBoot.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
